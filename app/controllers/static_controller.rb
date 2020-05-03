df835740d86b2568bac5d8f8f0f335f5916d55b3
jun sunami <j.s.d.minkey@gmail.com>
2020-05-03 21:28:54 +0900

    rspecのインストールから設定(既に作成しているViewファイル等はまだ)

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
   def homepage
   end
+
+  def features
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static#homepage'
+  get 'features', to: 'static#features'
 end
