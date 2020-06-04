32e6f2c129baef4633b71b0bfbf1b4ef847f115d
jun sunami <j.s.d.minkey@gmail.com>
2020-06-04 19:56:43 +0900

    ホーム画面の合計値を、作業完了しているものだけ取得して合計する

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,7 @@
 class StaticController < ApplicationController
   def homepage
     if user_signed_in? 
-      today = Date.today
-      this_month = today.all_month
-      user_score = Post.group(:user_id)
+      user_score = Post.group(:user_id).where(status: "完了")
       @all_elapsed_time = user_score.sum(:elapsed_time)
       @all_hourly_wage = user_score.average(:hourly_wage)
       @all_price = user_score.sum(:price)
