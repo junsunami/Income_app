251a1c7f8a114e9c5932742926b9ba4e110bb0c3
jun sunami <j.s.d.minkey@gmail.com>
2020-06-14 17:30:01 +0900

    post部分んテストを記述

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 describe 'Get# Post' do
     let(:user) { create(:user) }
+
     before do
         login_as(user, :scope => :user)
 
         visit root_path
     end
 
-    it 'ホーム画面' do
-        expect(page).to have_content('累計稼働時間')
+    it 'ホーム画面正常に表示(Postデータ無し)' do
+        expect(page).to have_content('Hourly_wageへようこそ')
     end
 
-    describe 'index' do
-        it 'show User 時給ログ' do
-            visit posts_path
-            expect(page).to have_content('時給ログ')
-        end
+    it 'ホーム画面正常に表示(Postデータ有り)' do
+        post = Post.create(description: "記事", price: "100", elapsed_time: 3600, hourly_wage: 100, user_id: user.id, status: 2)
+        visit root_path
+        expect(page).to have_content('累計稼働時間')
     end
 end
